85e2c086c4529147e5559e79b2ac02e017f0648a
nokkiiess <nokkiiess@gmail.com>
2023-09-22 14:25:07 +0700

    useMemo-useEffect

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, memo, useMemo, useCallback } from "react";
+import "./App.css";
+
+function Swatch({ params, onClick }) {
+  console.log("Swatch Render", params.color);
+  return (
+    <div
+      style={{
+        margin: 2,
+        width: 75,
+        height: 75,
+        backgroundColor: params.color,
+      }}
+      onClick={onClick}
+    ></div>
+  );
+}
+
+const MemoedSwatch = memo(Swatch);
 
 function App() {
+  const [appRenderIndex, setAppRenderIndex] = useState(0);
+  const [color, setColor] = useState("red");
+
+  console.log("App Rebder Index", appRenderIndex);
+
+  const params = useMemo(() => ({ color }), [color]);
+  const onClick = useCallback(() => {}, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <button onClick={() => setAppRenderIndex(appRenderIndex + 1)}>
+          Re-Render App
+        </button>
+      </div>
+      <div>
+        <button onClick={() => setColor(color === "red" ? "blue" : "red")}>
+          Change Color
+        </button>
+      </div>
+      <div>
+        <MemoedSwatch params={params} onClick={onClick} />
+      </div>
     </div>
   );
 }
